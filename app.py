ae134fd071166959fea1ad245aab0a82e1674391
Evan Mazor <evanmazor@gmail.com>
2021-03-25 20:46:37 -0700

    Heroku fix 2

--- app.py
+++ app.py
@@ -2,16 +2,17 @@ from flask import Flask
 import text
 import voice
 
-if __name__ == '__main__':
-    app = Flask(__name__)
+app = Flask(__name__)
 
-    @app.route('/', methods=['GET', 'POST'])
-    def root():
-        return 'Covidwa Notifications'
 
-    app.route('/text', methods=['GET', 'POST'])(text.respond)
+@app.route('/', methods=['GET', 'POST'])
+def root():
+    return 'Covidwa Notifications'
 
-    app.route('/voice', methods=['GET', 'POST'])(voice.answer)
-    app.route('/gather', methods=['GET', 'POST'])(voice.gather)
 
-    app.run(debug=True, host='0.0.0.0')
+app.route('/text', methods=['GET', 'POST'])(text.respond)
+
+app.route('/voice', methods=['GET', 'POST'])(voice.answer)
+app.route('/gather', methods=['GET', 'POST'])(voice.gather)
+
+app.run(debug=True, host='0.0.0.0')
