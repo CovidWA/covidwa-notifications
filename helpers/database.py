de371884a613c860713d628b8a1d7f858dd38333
Evan Mazor <evanmazor@gmail.com>
2021-03-31 20:26:57 -0700

    Caching

--- helpers/database.py
+++ helpers/database.py
@@ -1,6 +1,8 @@
 from dotenv import load_dotenv
 from firebase import FirebaseApplication
+import json
 import os
+from time import time
 
 
 class Database:
@@ -10,8 +12,24 @@ class Database:
         self.firebase = FirebaseApplication(url, None)
         self.params = {'auth': os.environ['FIREBASE_AUTH']}
 
+    def cache(self):
+        print('caching...')
+        with open('users_cache.json', 'w') as f:
+            d = {
+                'data': self.firebase.get('users', None, params=self.params),
+                'time': time()
+            }
+            json.dump(d, f)
+        return d
+
     def get(self):
-        return self.firebase.get('users', None, params=self.params)
+        if not os.path.exists('users_cache.json'):
+            self.cache()
+        with open('users_cache.json', 'r') as f:
+            d = json.load(f)
+            if time() - d['time'] > 60:  # If old data
+                d = self.cache()
+            return d['data']
 
     def post(self, phone_number, zip_code):
         d = {'phone_number': phone_number, 'zip_code': zip_code}
